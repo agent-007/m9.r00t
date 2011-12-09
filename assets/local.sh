07801ec0533af41cbc5da1959c6fc133cd7135c5
Alex Gorbachenko <agent_007@aspire-3820tg.(none)>
2011-12-09 23:12:22 +0400

    make these works for 10373 firmware

--- assets/local.sh
+++ assets/local.sh
@@ -1,18 +1,31 @@
 #!/system/bin/sh -x
-test -x /system/xbin/su
+
+test -x /system/bin/su
 if [ "$?" == 0 ] ; then 
     echo "already r00ted"
     exit 0
 fi
 
 echo "running from uid 0..."
-echo "mount /system/xbin to memory..."
-mount -t tmpfs none /system/xbin
-echo "installing busybox and su..."
+
+echo "prepare busybox and su..."
 chmod 4755 /data/data/ru.meizu.m9.r00t/files/busybox
 chmod 4755 /data/data/ru.meizu.m9.r00t/files/su
-/data/data/ru.meizu.m9.r00t/files/busybox --install -s /system/xbin
+
+echo "store original /system/bin to new path..."
+/data/data/ru.meizu.m9.r00t/files/busybox mkdir -p /data/data/ru.meizu.m9.r00t/files/system/bin
+/data/data/ru.meizu.m9.r00t/files/busybox mount -o bind /system/bin /data/data/ru.meizu.m9.r00t/files/system/bin
+
+echo "mount /system/bin into memory..."
+/data/data/ru.meizu.m9.r00t/files/busybox mount -t tmpfs none /system/bin
+
+echo "link original /system/bin content to new location..."
+/data/data/ru.meizu.m9.r00t/files/busybox ln -s /data/local/system/bin/* /system/bin/
+
+echo "installing busybox and su..."
+/data/data/ru.meizu.m9.r00t/files/busybox --install -s /system/bin
 /data/data/ru.meizu.m9.r00t/files/busybox cp /data/data/ru.meizu.m9.r00t/files/su /system/xbin/su
+
 if [ "$?" == 0 ] ; then
     echo "done. now you have temporary root rights."
 else
--- assets/unroot.sh
+++ assets/unroot.sh
@@ -1,5 +1,5 @@
 #!/system/bin/sh -x
-/system/xbin/su -c '/system/bin/umount /system/xbin' &&
+/system/xbin/su -c '/system/bin/umount /system/bin' &&
 echo "device unr00ted."
 echo "to obtain r00t rights touch Get r00t."
 
