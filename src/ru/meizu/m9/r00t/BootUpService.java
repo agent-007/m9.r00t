b2da00c54c9ee4b75443ae547b4c535052ebaa41
agent_007 <agent_007@immo.ru>
2011-12-09 13:56:10 +0400

    hehe

--- src/ru/meizu/m9/r00t/BootUpService.java
+++ src/ru/meizu/m9/r00t/BootUpService.java
@@ -1,5 +1,12 @@
 package ru.meizu.m9.r00t;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -8,8 +15,11 @@ import android.util.Log;
 import android.view.Gravity;
 import android.widget.Toast;
 
+<<<<<<< HEAD
 import static android.util.Log.e;
 
+=======
+>>>>>>> parent of 12f4884... organize code
 public class BootUpService extends Service {
 
     private final CommonTasks commonTasks = new CommonTasks();
@@ -20,7 +30,7 @@ public class BootUpService extends Service {
 
 	public int onStartCommand(Intent intent, int flags, int startId) {
 
-		Log.i("m9.r00t: onStartCommand", "Received start id " + startId + ": " + intent);
+		Log.i("m9.r00t", "Received start id " + startId + ": " + intent);
 
 		commonTasks.copyfile("su");
         commonTasks.copyfile("busybox");
@@ -56,7 +66,6 @@ public class BootUpService extends Service {
 		try {
 			basedir = getBaseContext().getFilesDir().getAbsolutePath();
 		} catch (Exception e) {
-            e("m9.r00t: RunExploit", "Can't find basedir");
 		}
 
 		@SuppressWarnings("unused")
@@ -76,4 +85,58 @@ public class BootUpService extends Service {
 
 	}
 
+<<<<<<< HEAD
+=======
+	// Executes UNIX command.
+	private String exec(String command) {
+		try {
+			Process process = Runtime.getRuntime().exec(command);
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					process.getInputStream()));
+			int read;
+			char[] buffer = new char[4096];
+			StringBuffer output = new StringBuffer();
+			while ((read = reader.read(buffer)) > 0) {
+				output.append(buffer, 0, read);
+			}
+			reader.close();
+			process.waitFor();
+			return output.toString();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private void copyfile(String file) {
+		String basedir = null;
+		String of = file;
+		File f = new File(of);
+
+		try {
+			basedir = getBaseContext().getFilesDir().getAbsolutePath();
+		} catch (Exception e) {
+		}
+
+		if (!f.exists()) {
+			try {
+				InputStream in = getAssets().open(file);
+				FileOutputStream out = getBaseContext().openFileOutput(of,
+						MODE_PRIVATE);
+
+				byte[] buf = new byte[1024];
+				int len;
+				while ((len = in.read(buf)) > 0) {
+					out.write(buf, 0, len);
+				}
+				out.close();
+				in.close();
+				Runtime.getRuntime().exec("chmod 755 " + basedir + "/" + of);
+			} catch (IOException e) {
+			}
+		}
+	}
+
+>>>>>>> parent of 12f4884... organize code
 }
