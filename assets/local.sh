a8b89ca871aea6b28f201c28870cb2771304def0
Alex Gorbachenko <agent_007@aspire-3820tg.(none)>
2011-12-10 01:45:14 +0400

    adfree support

--- assets/local.sh
+++ assets/local.sh
@@ -1,5 +1,7 @@
 #!/system/bin/sh -x
 
+BASEDIR="/data/data/ru.meizu.m9.r00t/files"
+
 test -x /system/bin/su
 if [ "$?" == 0 ] ; then 
     echo "already r00ted"
@@ -9,22 +11,41 @@ fi
 echo "running from uid 0..."
 
 echo "prepare busybox and su..."
-chmod 4755 /data/data/ru.meizu.m9.r00t/files/busybox
-chmod 4755 /data/data/ru.meizu.m9.r00t/files/su
+chmod 4755 "$BASEDIR"/busybox
+chmod 4755 "$BASEDIR"/su
 
 echo "store original /system/bin to new path..."
-/data/data/ru.meizu.m9.r00t/files/busybox mkdir -p /data/data/ru.meizu.m9.r00t/files/system/bin
-/data/data/ru.meizu.m9.r00t/files/busybox mount -o bind /system/bin /data/data/ru.meizu.m9.r00t/files/system/bin
+"$BASEDIR"/busybox mkdir -p "$BASEDIR"/system/bin
+"$BASEDIR"/busybox mount -o bind /system/bin "$BASEDIR"/system/bin
+
+echo "store original /system/etc to new path..."
+"$BASEDIR"/busybox mkdir -p "$BASEDIR"/system/etc
+"$BASEDIR"/busybox mount -o bind /system/bin "$BASEDIR"/system/etc
 
 echo "mount /system/bin into memory..."
-/data/data/ru.meizu.m9.r00t/files/busybox mount -t tmpfs none /system/bin
+"$BASEDIR"/busybox mount -t tmpfs none /system/bin
 
 echo "link original /system/bin content to new location..."
-/data/data/ru.meizu.m9.r00t/files/busybox ln -s /data/data/ru.meizu.m9.r00t/files/system/bin/* /system/bin/
+"$BASEDIR"/busybox ln -s "$BASEDIR"/system/bin/* /system/bin/
+
+echo "mount /system/etc into memory..."
+"$BASEDIR"/busybox mount -t tmpfs none /system/etc
+
+echo "link original /system/etc content to new location..."
+"$BASEDIR"/busybox ln -s "$BASEDIR"/system/etc/* /system/etc/
 
 echo "installing busybox and su..."
-/data/data/ru.meizu.m9.r00t/files/busybox --install -s /system/bin
-/data/data/ru.meizu.m9.r00t/files/busybox cp /data/data/ru.meizu.m9.r00t/files/su /system/bin/su
+"$BASEDIR"/busybox --install -s /system/bin
+"$BASEDIR"/busybox cp "$BASEDIR"/su /system/bin/su
+
+echo "check adfree files in /data..." 
+test -f /data/data/hosts
+if [ "$?" == 0 ] ; then
+    "$BASEDIR"/busybox ln -s /data/data/hosts /system/etc/
+    echo "change system /etc/hosts to adfree version..."
+else
+    echo "adfree is not installed or not configured properly."
+fi
 
 if [ "$?" == 0 ] ; then
     echo "done. now you have temporary root rights."
--- assets/unroot.sh
+++ assets/unroot.sh
@@ -1,5 +1,6 @@
 #!/system/bin/sh -x
-/system/xbin/su -c '/system/bin/umount /system/bin' &&
+/system/bin/su -c '/data/data/ru.meizu.m9.r00t/files/busybox umount /system/bin' &&
+/system/bin/su -c '/data/data/ru.meizu.m9.r00t/files/busybox umount /system/etc' &&
 echo "device unr00ted."
 echo "to obtain r00t rights touch Get r00t."
 
