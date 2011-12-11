2b8275f366250f08bbf04a47fd99f2be265e1b9f
Alex Gorbachenko <agent_007@aspire-3820tg.(none)>
2011-12-11 22:21:43 +0400

    - v1.1.6; - revert root shell script;

--- assets/local.sh
+++ assets/local.sh
@@ -2,7 +2,7 @@
 
 BASEDIR="/data/data/ru.meizu.m9.r00t/files"
 
-test -x /system/xbin/su
+test -x /system/bin/su
 if [ "$?" == 0 ] ; then 
     echo "already r00ted"
     exit 0
@@ -14,26 +14,37 @@ echo "prepare busybox and su..."
 chmod 4755 "$BASEDIR"/busybox
 chmod 4755 "$BASEDIR"/su
 
-echo "store original /system  to new path..."
-"$BASEDIR"/busybox rm -r "$BASEDIR"/system
-"$BASEDIR"/busybox mkdir -p "$BASEDIR"/system
-"$BASEDIR"/busybox mount -o bind /system "$BASEDIR"/system
+echo "store original /system/bin to new path..."
+"$BASEDIR"/busybox mkdir -p "$BASEDIR"/system/bin
+"$BASEDIR"/busybox mount -o bind /system/bin "$BASEDIR"/system/bin
 
-echo "mount /system into memory..."
-"$BASEDIR"/busybox mount -t tmpfs none /system
-#echo "link original /system  content to new location..."
-"$BASEDIR"/busybox ln -s "$BASEDIR/system/*" /system/
+
+echo "mount /system/bin into memory..."
+"$BASEDIR"/busybox mount -t tmpfs none /system/bin
+
+echo "link original /system/bin content to new location..."
+"$BASEDIR"/busybox ln -s "$BASEDIR"/system/bin/* /system/bin/
+
 
 echo "installing busybox and su..."
-"$BASEDIR"/busybox mkdir -p /system/xbin
-"$BASEDIR"/busybox --install -s /system/xbin
-"$BASEDIR"/busybox cp "$BASEDIR"/su /system/xbin/su
+"$BASEDIR"/busybox --install -s /system/bin
+"$BASEDIR"/busybox cp "$BASEDIR"/su /system/bin/su
 
-echo "check adfree /etc/hosts file in /data..." 
+echo "check adfree files in /data..." 
 test -f /data/data/hosts
 if [ "$?" == 0 ] ; then
+    echo "store original /system/etc to new path..."
+    "$BASEDIR"/busybox mkdir -p "$BASEDIR"/system/etc
+    "$BASEDIR"/busybox mount -o bind /system/bin "$BASEDIR"/system/etc
+
+    echo "mount /system/etc into memory..."
+    "$BASEDIR"/busybox mount -t tmpfs none /system/etc
+
+    echo "link original /system/etc content to new location..."
+    "$BASEDIR"/busybox ln -s "$BASEDIR"/system/etc/* /system/etc/
+
+    "$BASEDIR"/busybox ln -s /data/data/hosts /system/etc/
     echo "change system /etc/hosts to adfree version..."
-    "$BASEDIR"/busybox cp -sf /data/data/hosts /system/etc/
 else
     echo "adfree is not installed or not configured properly."
 fi
