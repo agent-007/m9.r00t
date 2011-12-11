57898810e18deebdde3cf729edc5ead4b82189b1
Alex Gorbachenko <agent_007@aspire-3820tg.(none)>
2011-12-11 23:06:34 +0400

    - v1.1.6; - fix unroot;

--- assets/unroot.sh
+++ assets/unroot.sh
@@ -2,11 +2,10 @@
 BASEDIR="/data/data/ru.meizu.m9.r00t/files"
 chmod 4755 "$BASEDIR/su"
 
-"$BASEDIR/su" -c "$BASEDIR/busybox umount -l /system/etc"
-"$BASEDIR/su" -c "$BASEDIR/busybox umount -l /data/data/ru.meizu.m9.r00t/files/system/etc"
+su -c "$BASEDIR/busybox umount -l /system/etc"
+su -c "$BASEDIR/busybox umount -l /data/data/ru.meizu.m9.r00t/files/system/etc"
 
-"$BASEDIR/su" -c "$BASEDIR/busybox umount -l /system/bin"
-"$BASEDIR/su" -c "$BASEDIR/busybox umount -l /data/data/ru.meizu.m9.r00t/files/system/bin"
+su -c "$BASEDIR/busybox umount -l /system/bin; $BASEDIR/busybox umount -l /data/data/ru.meizu.m9.r00t/files/system/bin"
 
 echo "device unr00ted."
 echo "to obtain r00t rights touch Get r00t."
