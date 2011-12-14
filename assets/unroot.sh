11ef35e8837053657697b1b051423f1f08b18e12
Alex <alex.gorbachenko@gmail.com>
2011-12-14 14:02:12 +0400

    - prepare to android market

--- assets/unroot.sh
+++ assets/unroot.sh
@@ -1,10 +1,10 @@
 #!/system/bin/sh -x
 BASEDIR="/data/data/ru.meizu.m9.r00t/files"
 
+su -c "mount -w -o remount -t rootfs rootfs /"
 su -c "$BASEDIR/busybox umount -l /system/etc"
 su -c "$BASEDIR/busybox umount -l /data/data/ru.meizu.m9.r00t/files/system/etc"
-
-su -c "$BASEDIR/busybox rm -f /sbin/su"
+su -c "$BASEDIR/busybox rm -f /sbin/su ; mount -r -o remount -t rootfs rootfs /"
 
 #su -c "$BASEDIR/busybox umount -l /system/bin; $BASEDIR/busybox umount -l /data/data/ru.meizu.m9.r00t/files/system/bin"
 
